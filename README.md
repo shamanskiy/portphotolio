c3befad170604540acaa2ee4696f5969114a23e2
Shamanskiy <shamanskiy.alexander@gmail.com>
2019-06-14 20:58:18 +0200

    new repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# portphotolio
+This is a simple online portfolio I am building for my girlfriend.
+The front end is written with HTML5/CSS3 and a little pinch of JavaScript.
+The back end is based on Flask, a Python web framework.
+The portfolio is currently hosted on the Heroku platform and can be accessed
+via [https://sophiesophie.herokuapp.com](https://sophiesophie.herokuapp.com/).
---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/white_sea')
+def white_sea():
+    return render_template("white_sea.html")
+
+@app.route('/crimea')
+def crimea():
+    return render_template("crimea.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    #app.run(host='131.264.70.53',port = "5010")
