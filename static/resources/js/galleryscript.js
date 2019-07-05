c5245b4f3e71462f694d5bb9d91ff63dd7210c18
Shamanskiy <shamanskiy.alexander@gmail.com>
2019-07-05 22:44:44 +0200

    second album viewer done

--- static/resources/js/galleryscript.js
+++ static/resources/js/galleryscript.js
@@ -1,4 +1,4 @@
-var initPhotoSwipeFromDOM = function(gallerySelector) {
+var initPhotoSwipe = function(gallerySelector) {
 
     var parseGallery = function(gallery) {
         var figures = gallery.getElementsByTagName('figure');
@@ -24,23 +24,19 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
             slide.el = figure; // save link to element for getThumbBoundsFn
             slides.push(slide);
         }
-
         return slides;
     };
 
-    // find nearest parent element
     var closest = function closest(el, fn) {
         return el && ( fn(el) ? el : closest(el.parentNode, fn) );
     };
 
-    // triggers when user clicks on thumbnail
     var onThumbnailsClick = function(e) {
         e = e || window.event;
         e.preventDefault ? e.preventDefault() : e.returnValue = false;
 
         var eTarget = e.target || e.srcElement;
 
-        // find root element of slide
         var clickedListItem = closest(eTarget, function(el) {
             return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
         });
@@ -49,10 +45,6 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
             return;
         }
 
-        
-        
-        // find index of clicked item by looping through all child nodes
-        // alternatively, you may define index via data- attribute
         var clickedGallery = clickedListItem.parentNode.parentNode.parentNode,
             nodeIndex = 0,
             index;
@@ -68,7 +60,6 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
         }
 
         if(index >= 0) {
-            // open PhotoSwipe if valid index found
             openPhotoSwipe( index, clickedGallery );
         }
         return false;
@@ -78,56 +69,28 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
         var pswpElement = document.querySelectorAll('.pswp')[0],
             gallery,
             options,
-            items;
+            slides;
 
-        items = parseGallery(galleryElement);
+        slides = parseGallery(galleryElement);
 
-        // define options (if needed)
         options = {
-
-            // define gallery index (for URL)
-            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
-
             getThumbBoundsFn: function(index) {
-                // See Options -> getThumbBoundsFn section of documentation for more info
-                var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+                var thumbnail = slides[index].el.getElementsByTagName('img')[0], // find thumbnail
                     pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
                     rect = thumbnail.getBoundingClientRect(); 
-
                 return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
             }
-
         };
 
-        // PhotoSwipe opened from URL
-
-            options.index = parseInt(index, 10);
-
-
-        // exit if index not found
-        if( isNaN(options.index) ) {
-            return;
-        }
-
-
-            options.showAnimationDuration = 0;
+        options.showAnimationDuration = 0;
         options.hideAnimationDuration = 0;
 
-
-        // Pass data to PhotoSwipe and initialize it
-        gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery = new PhotoSwipe(pswpElement,PhotoSwipeUI_Default,slides,options);
         gallery.init();
     };
 
-    // loop through all gallery elements and bind events
-    var galleryElements = document.querySelectorAll( gallerySelector );
-
-    for(var i = 0, l = galleryElements.length; i < l; i++) {
-        galleryElements[i].setAttribute('data-pswp-uid', i+1);
-        galleryElements[i].onclick = onThumbnailsClick;
-    }
+    var galleryElements = document.querySelectorAll(gallerySelector);
+    galleryElements[0].onclick = onThumbnailsClick;    
 };
 
-// execute above function
-
-initPhotoSwipeFromDOM('.section-photos');
+initPhotoSwipe('.section-photos');
